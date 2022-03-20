be6969478b9455fb2e5b2422909ae5d9c52404ab
nehachougule1998 <nchougule734@gmail.com>
2022-03-20 09:44:37 +0530

    UC2 added

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,39 @@
+package com.bridgelabz;
+
+public class LinkedList {
+
+	Node head;
+	Node tail;
+	
+	public void add(int data) {
+		Node newNode = new Node(data);
+		if(head == null) {
+			head = newNode;
+			tail = newNode;
+			return;
+		}
+		else {
+			tail.next = newNode;
+			tail = newNode;
+		}	
+	}
+	
+	public void show() {
+		Node  temp = head;
+		while(temp != null) {
+			System.out.print(temp.data +" ");
+			temp = temp.next;
+		}
+	}
+}
+
+class Node {
+	
+	int data;
+	Node next;
+	
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+public class LinkedListMain {
+	
+public static void main(String[] args) {
+		System.out.println("welcome to linked list program");
+		LinkedList list = new LinkedList();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		list.show();
+	}
+
+}
