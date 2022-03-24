351922fef692d878c825dbedae7f4f8fef7bf22a
nehachougule1998 <nchougule734@gmail.com>
2022-03-24 13:18:38 +0530

    UC3 added

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -1,43 +1,39 @@
 package com.bridgelabz;
 
 public class LinkedListMain {
-	
-	public static void main(String[] args) {
 
-		LinkedList list = new LinkedList();
-		list.append(56);
-		list.append(30);
-		list.append(70);
-		list.show();
-		list.sortList();
-		System.out.println();
-		list.show();
-		System.out.println();
-		list.insertAtMiddle(30);
-		list.show();	
-		System.out.println();
-		list.popFirst();
-		list.show();
-		list.popLast();
-		list.show();
-		System.out.println();
-		if(list.searchNode(30)) {
-			System.out.println("serach key found");
-		}
-		else {
-			System.out.println("serach key not found");
-		}
-		
-		System.out.println();
-		list.insertAtPosition(40, 3);
-		list.show();
-		
-		System.out.println();
-		list.removeElements(40);
-		list.show();
-		System.out.println(list.getSize());
+	LinkedList list = new LinkedList();
+	list.append(56);
+	list.append(30);
+	list.append(70);
+	list.show();
+	list.sortList();
+	System.out.println();
+	list.show();
+	System.out.println();
+	list.insertAtMiddle(30);
+	list.show();	
+	System.out.println();
+	list.popFirst();
+	list.show();
+	list.popLast();
+	list.show();
+	System.out.println();
+	if(list.searchNode(30)) {
+		System.out.println("serach key found");
 	}
-
+	else {
+		System.out.println("serach key not found");
+	}
+	
+	System.out.println();
+	list.insertAtPosition(40, 3);
+	list.show();
+	
+	System.out.println();
+	list.removeElements(40);
+	list.show();
+	System.out.println(list.getSize());
 }
 
-
+}
---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class Main {
+
+public static void main(String[] args) {
+	
+	Queue queue = new Queue();
+	
+	queue.append(56);
+	queue.append(30);
+	queue.append(70);
+	queue.show();
+}
+}
